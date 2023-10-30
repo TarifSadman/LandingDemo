0d6f244396d4bb3f473bd81fd76581d61d3fe251
TarifSadman <sadmantaha17@gmail.com>
2023-10-30 16:51:31 +0600

    new commit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -48,8 +48,7 @@ const Contact = (props) => {
               <div className="section-title">
                 <h2>Get In Touch</h2>
                 <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
+                  Kindly complete the form provided below to send us an email, and we will respond to your inquiry as swiftly as we can.
                 </p>
               </div>
               <form name="sentMessage" validate onSubmit={handleSubmit}>
