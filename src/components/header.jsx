5f16c2b49d68b00c65d16bb9699f5b9526a66baf
TarifSadman <sadmantaha17@gmail.com>
2023-10-30 16:41:41 +0600

    commit

---
+++ src/components/header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Zoom } from "react-reveal";
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  {props.data ? props.data.title : "Loading"}
+                  <span></span>
+                </h1>
+                <Zoom clear duration={1000} delay={1000}>
+                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                </Zoom>
+                <a
+                  href="#features"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Learn More
+                </a>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ src/emailjs.js
@@ -0,0 +1,7 @@
+// EMAILJS_SERVICE_ID = 'service_r1u9jey'
+// EMAILJS_TEMPLATE_ID = 'template_3v53gwl'
+// EMAILJS_OTHER = '9UWNC4t8mUEi1v5zQ'
+
+export const EMAILJS_SERVICE_ID = 'service_r1u9jey'
+export const EMAILJS_TEMPLATE_ID = 'template_3v53gwl'
+export const EMAILJS_OTHER = '9UWNC4t8mUEi1v5zQ'
